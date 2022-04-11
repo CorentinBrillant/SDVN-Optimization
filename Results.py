6a61fe381f5c19b0fdabfbc99f63a836cc2a7f47
CorentinBrillant <65302240+CorentinBrillant@users.noreply.github.com>
2022-04-11 15:33:56 +0100

    Update

---
+++ Results.py
@@ -0,0 +1,71 @@
+#coding:utf-8
+
+from fctObjectives import *
+from datetime import datetime
+import numpy as np
+
+mapWidth = 7000
+mapHeight= 6000
+nbVoitures = 100
+
+#Initialisation des RSUs
+RSUs = []
+for i in range(5):
+	for j in range(6):
+		if i%2==0:
+			RSUs.append(Rsu(600+j*1100,600+i*1100,datetime.now(),0))
+		else:
+			if j not in [1,3,4]:
+				RSUs.append(Rsu(600+j*1100,600+i*1100,datetime.now(),0))
+
+# Initialisation des voitures    
+voitures_tmp = []
+for i in range(nbVoitures):
+    voitures_tmp.append(Voiture(datetime.now(),randrange(14)))
+
+# attribution des RSUs aux véhicules
+for v in voitures_tmp:
+	v.attrClosestRsu(RSUs)
+
+# on ne prend en compte que les véhicules à portée d'un RSU
+voitures = []
+for v in voitures_tmp:
+	if v.rsuAtr in range(24):
+		voitures.append(v)
+
+# affectation des RSUs non contrôleurs aux RSUs contrôleurs
+def choseControllerForEachRsu():
+	D = calcDists(RSUs)
+	RSU_Controller = [rsu for rsu in RSUs if rsu.isCont]
+	for rsu in RSUs:
+		if rsu.isCont == 0:
+			d_min = mapWidth*mapHeight
+			i_min = rsu.id
+			for c in RSU_Controller:
+				if D[rsu.id,c.id] < d_min:
+					i_min = c.id
+			if i_min != rsu.id:
+				rsu.cAtr = i_min
+
+# à partir d'un individu de notre population on configure l'implémentation objet du SDVN
+def particleToObjects(vect):
+
+	#on vérifie que la particle a autant de valeurs qu'il y a de RSUs
+	assert len(vect)==24
+	for i in range(len(RSUs)):
+		RSUs[i].isCont = vect[i]
+
+	# on affecte les RSUs non contrôleurs aux RSUs contrôleurs
+	choseControllerForEachRsu()
+
+	# on calcule les 4 objectifs 
+	values = []
+	values.append(clockSync(RSUs, voitures))
+	values.append(nbControleurs(RSUs))
+	values.append(latency(RSUs, voitures))
+	values.append(equiCharge(RSUs, voitures))
+
+	return values
+
+
+print(particleToObjects((np.random.rand(24) > 0.5).astype(int)))
--- fctObjectives.py
+++ fctObjectives.py
@@ -4,9 +4,11 @@ import math
 import uuid
 from datetime import datetime
 
+mapWidth = 7000
+mapHeight= 6000
 T = 4000 #taille total de la map T*T
 p= -1 #Coefficient p pour le calcul du coefficient C
-nbVoitures = 100#Le nombre total de voitures
+nbVoitures = 100 #Le nombre total de voitures
 nbRsu = 24 #Le nombre total de RSU
 basicID = str(uuid.uuid4()) #basic ID pour les initialisations d'objets   
 S = 500 #taille de couverture
@@ -20,19 +22,31 @@ def distance(item1,item2):
 
 #Classe pour les voitures      
 class Voiture:
+
+    id = 0
+
     def __init__(self,H,vitesse):
         self.h = H 
         self.v = vitesse
         self.rsuAtr = basicID
-        self.x = randrange(T)
-        self.y = randrange(T)
+        self.x = randrange(mapWidth)
+        self.y = randrange(mapHeight)
+        self.id = Voiture.id
+        Voiture.id += 1
         
-    #méthode pour attribruer un controleur  
+    #méthode pour attribruer un RSU  
     def attrRsu(self,rsu):
         if distance(self,rsu) <= S:
             self.rsuAtr = rsu.id
+            return 1
         else:
-            print("le rsu est trop loin")
+            return 0
+
+    # attribue au véhicule le premier RSU à portée dans la liste RSUs
+    def attrClosestRsu(self, RSUs):
+        for rsu in RSUs:
+            if self.attrRsu(rsu)==1:
+                break
 
     def printVoiture(self):
         print("h = ",self.h.second)
@@ -44,18 +58,23 @@ class Voiture:
 
 #Classe pour les RSU
 class Rsu:
-    def __init__(self,H,isControleur):
+
+    idRSU = 0
+
+    def __init__(self,x,y,H,isControleur):
         self.hRsu = H 
-        self.x = randrange(T)
-        self.y = randrange(T)
+        self.x = x #randrange(T)
+        self.y = y #randrange(T)
         self.isCont = isControleur
         self.cAtr = basicID
         self.z = 0
         self.coef = 0
         self.s = S
         self.novi = 0
-        self.id = str(uuid.uuid4())
+        self.id = Rsu.idRSU
+        Rsu.idRSU += 1
         self.nbVois = 0
+
     #Méthode string des rsu
     def printRsu(self):
         print("h = ",self.hRsu.second)
@@ -67,156 +86,59 @@ class Rsu:
         print("novi = ",self.novi)
         print("id = ",self.id)
         print("nombre de voisins",self.nbVois)
-    
-    #Méthode pour mettre a jour le nombre voisins
-    def majVois(self,nb):
-        self.nbVois = nb
+
     
     #Calculer le coeff novi (nb de véhicule couvert par le rsu)
     def calcNovi(self,voitures):
         novi = 0
         for v in voitures:
-            if distance(self,v) <= S:
+            if v.rsuAtr == self.id:
                 novi = novi + 1
         self.novi = novi 
-    #Calculer le coeff z du rsu (si le rsu est un controleur z est le nombre de véhicule controlé par le rsu)
-    def calcZ(self,vs,rs):
-        for v in vs:
-            if (distance(self,v) < S):
-                if (self.isCont == 1):
-                    if v.rsuAtr == self.id: 
-                        self.z = self.z+1
-                else:
-                    for r in rs: 
-                        if r.id == self.cAtr:
-                            r.z = r.z+1
+
+    # on calcule le nb de voisins d'un RSU
+    def calcVois(self, rs):
+        count = 0
+        for rsu in rs:
+            if distance(self, rsu) < C and rsu.id != self.id:
+                count += 1
+        self.nbVois = count
                     
        
     #Méthode pour le calcul du coefficient C
-    def calcCoef (self,vMoy):
-        self.coef = p * ((nbVoitures*self.nbVois)/vMoy)
-    
-    #Méthode pour attribuer un controleur a un RSU
-    def attrCont(self,controleur,rs,vs):
-        if (distance(self,controleur) <= C): #Contrainte de la distance entre un rsu et son controleur
-            if controleur.isCont == 1:
-                self.cAtr = controleur.id
-                self.calcZ(vs,rs)
-            else:
-                print("ce rsu n'est pas un controleur")
+    def calcCoef (self, vs, rs):
+        vMoy = 0
+        for v in vs:
+            if v.rsuAtr == self.id:
+                vMoy += v.v
+        self.calcNovi(vs)
+        if self.novi <= 0:
+            self.coef = 0
         else:
-            print("non la contrainte de distance n'est pas respecté")
-    def devContr(self,vs,rs):
-        self.isCont = 1
-        self.calcZ(vs,rs)
-        
-        
-        
-#Initialisation des voitures    
-voitures = []
-for i in range(nbVoitures):
-    now = datetime.now()
-    v = Voiture(now,randrange(14))
-    voitures.append(v)
-    
-#On calcule la vitesse moyenne
-vitMoy = 0
-for v in voitures:
-    vitMoy = vitMoy + v.v
-vitMoy = vitMoy/nbVoitures
-
-#Initialisation des RSU
-rsus = []
-for i in range(nbRsu):
-    now = datetime.now()
-    r = Rsu(now,0)
-    rsus.append(r)
-
-#Initialisation de Dist la matrice des distances entre les RSU
-distInterRsus = np.zeros((nbRsu,nbRsu))
-def calcDists(rs,D):
-    for i in range(nbRsu):
-        for j in range(nbRsu):
-          D[i,j] = distance(rs[i],rs[j])  
-    return D
-
-distInterRsus = calcDists(rsus,distInterRsus)
-
-#On calcule le nb de voisins de chaque RSU
-for i in range(nbRsu):
-    vois = 0
-    for j in range(nbRsu):
-        if distInterRsus[i,j] < C:
-            vois = vois + 1
-    rsus[i].majVois(vois)
-
-#On calcule les coefficients C et novi de chacun des RSU
-for r in rsus:
-    r.calcCoef(vitMoy)
-    r.calcNovi(voitures)
-    
-    
-#Initialisation de distRsuVoit la matrice des distance entre les RSU et les voitures
-distRsuVoit = np.zeros((nbRsu,nbVoitures))
-def calcDistsRsuVoit(rs,vs,D):
-    for i in range(nbRsu):
-        for j in range(nbVoitures):
-          D[i,j] = distance(rs[i],vs[j])  
-    return D
-
-distRsuVoit = calcDistsRsuVoit(rsus,voitures,distRsuVoit)
-
-
-#Initialisation de X
-X = np.zeros((nbRsu,nbRsu))
-
-#Fonction pour mettre à jour X (on met à jour X a chaque fois qu'un rsu devient un controleur ou qu'un rsu est attribué à un controleur)
-def majMatCont(rs,matCont): 
-    for i in range(nbRsu):
-        for j in range(nbRsu):
-            if (i != j) and (rs[i].cAtr == rs[j].id) and (rs[i].cAtr != basicID):
-                matCont[i,j] = 1
+            vMoy = vMoy / self.novi
+            if vMoy>0:
+                self.calcVois(rs)
+                self.coef = p * ((self.novi*self.nbVois)/vMoy)
             else:
-                if (i == j) and (rs[i].isCont == 1):
-                    matCont[i,j] = 1
-                else:
-                    matCont[i,j] = 0
-    return matCont
-
-#faire devenir controleur un RSU
-def devenirContr(rsu,matCont,rs,vs):
-    rsu.devContr(vs,rs)
-    majMatCont(rs,matCont)
-
-#attribuer un controleur à un RSU
-def attribContr(rsu,cont,matCont,rs,vs):
-    rsu.attrCont(cont,rs,vs)
-    majMatCont(rs,matCont)
-
-#Initialisation de M
-M = np.zeros((nbVoitures,nbRsu))
-
-#Fonction pour mettre à jour M 
-def majMatM(rs,vs,M): 
-    for i in range(nbVoitures):
+                self.coef = 0
+        
+        
+#Initialisation de Dist la matrice des distances entre les RSU
+def calcDists(rs):
+    D = np.zeros((nbRsu,nbRsu))
+    for i in range(nbRsu):
         for j in range(nbRsu):
-            if (vs[i].rsuAtr == rs[j].id):
-                M[i,j] = 1
-            else: 
-                M[i,j]=0
-
-#Attribuer un RSU à une voiture
-def attribRsu(rsu,v,vs,rs,matM):
-    v.attrRsu(rsu)
-    majMatM(rs,vs,matM)
+            D[i,j] = distance(rs[i],rs[j])  
+    return D
 
 
 #Fonction pour la synchronization des horloges           
-def clockSync(M,vs,rs):
+def clockSync(rs, vs):
     sync = 0
-    for i in range(nbVoitures):
-        for j in range(nbRsu):
-            sync = sync + M[i,j]*abs(vs[i].h.second - rs[j].hRsu.second)
+    for i in range(len(vs)):
+        for j in range(len(rs)):
+            if vs[i].rsuAtr == j:
+                sync = sync + abs(vs[i].h.second - rs[j].hRsu.second)
     return sync
 
 
@@ -228,23 +150,32 @@ def nbControleurs(rs):
             c = c+1
     return c 
 
-
 #Fonction pour la latence 
-def latency(rs,D,matX):
+def latency(rs, vs):
+    D = calcDists(rs)
     lat = 0 
     for i in range(nbRsu):
         for j in range(nbRsu):
-            lat = lat +(((4*S) - D[i,j])*rs[i].coef*matX[i,j])
+            if rs[i].cAtr == j:
+                rs[i].calcCoef(vs,rs)
+                lat = lat +((C - D[i,j])*rs[i].coef)
     return lat
 
 
 #Fonction pour l'équité des charges
-def equiCharge(rs):
-    vecZ = []
-    for r in rs: 
-        vecZ.append(r.z)
-    zmin = min(vecZ)
-    zmax = max(vecZ)
+def equiCharge(rs, vs):
+    Z = []
+    for rsu in rs:
+        rsu.calcNovi(vs)
+    for rsu in rs:
+        if rsu.isCont:
+            Z_i = 0
+            for j in rs:
+                if j.isCont==0 and j.cAtr == rsu.id:
+                    Z_i += j.novi
+            Z.append(Z_i)
+    zmin = min(Z)
+    zmax = max(Z)
     return beta * (zmax - zmin)
 
 
